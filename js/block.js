085d2981e64d17727b58fe3dffe79f39cf743848
Jiyugata <luicalvn@gmail.com>
2016-03-09 12:33:09 -0800

    Update block.js

--- js/block.js
+++ js/block.js
@@ -1,19 +1,36 @@
 //Block cosntructor
-function Block(x, y, width, height){
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
-	this.getX = function(){
+function Block(x, y, width, height) {
+	/** Create Data Variables **/
+	this.x 		= x;
+	this.y 		= y;
+	this.width 	= width;
+	this.height 	= height;
+	
+	/********** Accessor **********/
+	this.getX = function() {
 		return this.x;
 	};
-	this.getY = function(){
+	this.getY = function() {
 		return this.y;
 	};
-	this.getWidth = function(){
+	this.getWidth = function() {
 		return this.width;
 	};
-	this.getHeight = function(){
+	this.getHeight = function() {
 		return this.height;
 	};
+	
+	/********** Mutators **********/
+	this.setX = function(x) {
+		this.x = x;
+	};
+	this.setY = function(y) {
+		this.y = y;
+	};
+	this.setWidth = function(width) {
+		this.width = width;
+	};
+	this.setHeight = function(height) {
+		this.height = height;
+	}
 }
