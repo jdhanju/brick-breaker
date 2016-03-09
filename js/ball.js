895fa25cac51e7ec34347bab9172828ffd227749
Jiyugata <luicalvn@gmail.com>
2016-03-09 12:34:17 -0800

    Update ball.js

--- js/ball.js
+++ js/ball.js
@@ -26,6 +26,18 @@ function Ball(x, y, radius, velX, velY, color) {
 	};
 	
 	/************** Mutators **************/
+	this.setX = function(x) {
+		this.x = x;
+	};
+	this.setY = function(y) {
+		this.y = y;
+	};
+	this.setWidth = function(width) {
+		this.width = width;
+	};
+	this.setHeight = function(height) {
+		this.height = height;
+	};
 	this.setVelX = function(velocityMod){ 
 		this.velX += velocityMod;
 	};
