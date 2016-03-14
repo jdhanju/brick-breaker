a47ae03d55b502655682f3ed77d40ab4c18eb327
zyrei <zyreigame@gmail.com>
2016-03-14 11:13:40 -0700

    Update breakerModel.js

--- js/breakerModel.js
+++ js/breakerModel.js
@@ -1,4 +1,11 @@
-
+/**
+ * Javeer Dhanju        
+ * Calvin Lui           
+ * Derek Yuan           100207884
+ * CPSC 1045            Term Project
+ *  
+ * -insert Description here-
+**/
 
 document.onkeypress = paddle;
 
