e6def16f01b6694e7264dbdd71e2f6249df0d98d
zyrei <zyreigame@gmail.com>
2016-03-14 11:14:06 -0700

    Update paddle.js

--- js/paddle.js
+++ js/paddle.js
@@ -1,3 +1,12 @@
+/**
+ * Javeer Dhanju        
+ * Calvin Lui           
+ * Derek Yuan           100207884
+ * CPSC 1045            Term Project
+ *  
+ * -insert Description here-
+**/
+
 var paddle = {
 	/***** Create Data Values *****/
 	x: 		550,
