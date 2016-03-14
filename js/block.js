2027b6ec5eb96a3db854ffc87fd52a3e54bcb8d5
zyrei <zyreigame@gmail.com>
2016-03-14 11:11:58 -0700

    Update block.js

--- js/block.js
+++ js/block.js
@@ -1,3 +1,12 @@
+/**
+ * Javeer Dhanju        
+ * Calvin Lui           
+ * Derek Yuan           100207884
+ * CPSC 1045            Term Project
+ *  
+ * -insert Description here-
+**/
+
 //Block cosntructor
 function Block(x, y, width, height, color) {
 	/** Create Data Variables **/
